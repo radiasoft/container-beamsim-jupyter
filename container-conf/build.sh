0d0b50ca3502d4d7e3fd1aefcaeadaa383fa62b0
Evan Carlin <evan@carlin.com>
2024-04-05 16:02:11 +0000

    Fix #2: use branches instead of commits

--- container-conf/build.sh
+++ container-conf/build.sh
@@ -3,16 +3,16 @@
 _beamsim_jupyter_build_vars() {
     : ${build_image_base:=radiasoft/beamsim-jupyter-base}
     build_is_public=1
-    build_passenv='PYKERN_COMMIT SIREPO_COMMIT'
-    : ${PYKERN_COMMIT:=} ${SIREPO_COMMIT:=}
+    build_passenv='PYKERN_BRANCH SIREPO_BRANCH'
+    : ${PYKERN_BRANCH:=} ${SIREPO_BRANCH:=}
 }
 
 build_as_run_user() {
     cd "$build_guest_conf"
-    _beamsim_jupyter_clone pykern "$PYKERN_COMMIT"
+    _beamsim_jupyter_clone pykern "$PYKERN_BRANCH"
     pip install .
     cd ..
-    _beamsim_jupyter_clone sirepo "$SIREPO_COMMIT"
+    _beamsim_jupyter_clone sirepo "$SIREPO_BRANCH"
     pip install -e .
     sirepo srw create_predefined
     pip install .
@@ -21,8 +21,8 @@ build_as_run_user() {
 
 _beamsim_jupyter_clone() {
     declare repo=$1
-    declare commit=$2
-    git clone -q -c advice.detachedHead=false ${commit:+--branch "$commit"} --depth=1 https://github.com/radiasoft/"$repo"
+    declare branch=$2
+    git clone -q -c advice.detachedHead=false ${branch:+--branch "$branch"} --depth=1 https://github.com/radiasoft/"$repo"
     cd $repo
 }
 
